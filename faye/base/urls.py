580897f376a5a13274ea7d7a49190b0855d8df2a
zmr <zmr@uchicago.edu>
2018-08-14 23:24:06 -0500

    enabled cv display.

--- faye/base/urls.py
+++ faye/base/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	path("", views.index, name="index"),
 	path("about/", views.about, name="about"),
 	path("contact/", views.contact, name="contact"),
+	path("cv/", views.cv, name="cv")
 ]
--- faye/base/views.py
+++ faye/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import FileResponse, Http404
 
 def index(request):
 	return render(request, 'base/index.html')
@@ -8,3 +9,9 @@ def about(request):
 
 def contact(request):
 	return render(request, 'base/contact_list.html')
+
+def cv(request):
+	try:
+		return FileResponse(open('static/base/img/zane_rossi_cv.pdf', 'rb'), content_type='application/pdf')
+	except FileNotFoundError:
+		raise Http404()
